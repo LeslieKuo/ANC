4d5c77fd3dbedb67d9af4ceabf543b7f00dc1681
LeslieKuo <676365950@qq.com>
2018-01-10 17:56:44 +0800

    learn how to r/w audio file.

---
+++ RW_audio.py
@@ -0,0 +1,17 @@
+# learn how to read and write .wav format audio
+from scipy.io import wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+
+orate,data = wavfile.read('kn.wav')
+print(orate,data)
+
+print(len(data))
+newdata=data[::-1]
+wavfile.write('test.wav',orate,newdata)
+
+
+N=20
+x=np.random.normal(0,1,(N,4))
+print(x)
+print(x[0,1])
